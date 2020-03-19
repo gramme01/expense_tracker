81c61a9f77bbf69cafd60269c4416e3a854f05a4
Emmanuel Oguntoyinbo <USER@SprinthubSpeed.local>
2020-03-19 04:04:02 +0100

    Alternative tocircleAvatar

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -36,12 +36,20 @@ class TransactionList extends StatelessWidget {
                   elevation: 5,
                   margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                   child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
+                    leading: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        shape: BoxShape.circle,
+                      ),
                       child: Padding(
                         padding: const EdgeInsets.all(6.0),
                         child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
+                          child: Text(
+                            '\$${transactions[index].amount}',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
                     ),
